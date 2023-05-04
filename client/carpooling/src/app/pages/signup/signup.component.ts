2bb7466b3061f89ab045d373afb5188beae749a5
jmimassi <jomimassi@gmail.com>
2023-05-04 03:19:10 +0200

    ajout de la redirection avec signin apres avoir fait un coimpte avec signup

--- client/carpooling/src/app/pages/signup/signup.component.ts
+++ client/carpooling/src/app/pages/signup/signup.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { UserService, User } from '../../services/user.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 
 @Component({
@@ -12,12 +14,21 @@ import { NgForm } from '@angular/forms';
 
 export class SignupComponent {
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private router: Router) { }
 
   onSubmit(users: { email: string; password: string, address: string, number_passengers_max: number, lisence_plate: string, picture: string }) {
     this.userService.userCreate(users).subscribe(
-      data => localStorage.setItem('token', data.token)
-    )
+      data => {
+        localStorage.setItem('token', data.token);
+        // form.reset(); // reset the form after successful submission
+        this.router.navigate(['/signin']); // navigate to dashboard page
+      },
+      error => {
+        console.log(error);
+      }
+    );
     console.log('users que je vois dans le signup component', users)
   }
+
+
 }
